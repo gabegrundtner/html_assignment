94a789bac6d5a5621100d2d412ca188c14243eb8
Gabe Grundtner <gbgrun10@smumn.edu>
2013-02-01 02:13:02 -0600

    html assignment complete

---
+++ app/controllers/assignment_controller.rb
@@ -0,0 +1,8 @@
+class AssignmentController < ApplicationController
+  def assignment
+  	puts "+++++++++"
+  end
+  def show
+  	puts "------------ "+params.inspect
+  end
+ end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HtmlAssignment::Application.config.secret_token = 'b9d4ae60abfd1bad4ebe4e40a4dd9b4844a60f7fc4909a5da7090d92d088cd68416e8b03a9104622bceeb47eef7b8f54641a6062bc7d2309cff49df1589ed234'
