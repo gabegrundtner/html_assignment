4e48bb366c1e8a2429e0212e75ac68864e4f2b7b
Gabe Grundtner <gbgrun10@smumn.edu>
2013-02-01 12:34:12 -0600

    hopefully final commit

--- app/controllers/assignment_controller.rb
+++ app/controllers/assignment_controller.rb
@@ -3,7 +3,14 @@ class AssignmentController < ApplicationController
   	puts "+++++++++"
   end
   def show
-  	#@name_age = params[:name][:name]
-  	puts "------------ "+params.inspect
+  	#puts "------------ "+params.inspect
+  	puts "-----------------"
+  	puts params[:name_or_age]
+  	if params[:check] == nil
+  		puts "not_checked"
+  	else
+  		puts params[:check]
+  	end
+  	puts params[:button]
   end
  end
